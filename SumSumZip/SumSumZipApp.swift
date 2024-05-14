875bcb7314c7444c02ce80bd1fc8f2ab4567789f
seongahshin <cindy903c@naver.com>
2024-05-14 14:23:36 +0900

    Initial Commit

---
+++ SumSumZip/SumSumZipApp.swift
@@ -0,0 +1,32 @@
+//
+//  SumSumZipApp.swift
+//  SumSumZip
+//
+//  Created by 신승아 on 5/14/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SumSumZipApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
