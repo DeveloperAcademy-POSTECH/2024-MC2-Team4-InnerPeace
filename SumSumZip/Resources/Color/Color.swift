d1d7e0536641ee647bc85affa84541971639c323
seongahshin <cindy903c@naver.com>
2024-05-15 14:19:36 +0900

    [Chore] 파일 구조화 및 PR 템플릿 생성

---
+++ SumSumZip/Resources/Color/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  SumSumZip
+//
+//  Created by 신승아 on 5/14/24.
+//
+
+import SwiftUI
+
+/// 앱 전체에서 사용되는 기본 색상을 정의합니다.
+/// 이 구조체를 통해 앱의 주요 색상 테마를 일관되게 관리할 수 있습니다.
+struct AppColors {
+    // 예시
+    static let primaryColor = Color(hex: "123456")
+    
+}
+
---
+++ SumSumZip/Resources/Strings/Strings.swift
@@ -0,0 +1,15 @@
+//
+//  Text.swift
+//  SumSumZip
+//
+//  Created by 신승아 on 5/14/24.
+//
+
+import Foundation
+
+/// 앱에서 사용되는 모든 문자열을 관리하는 구조체입니다.
+struct Strings {
+    // 예시
+    static let appName = "SumSumZip"
+    
+}
