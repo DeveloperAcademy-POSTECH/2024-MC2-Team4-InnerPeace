6d003c7862cc1aca210986901d23fde055f53234
heexohee <rsoy2918@naver.com>
2024-05-16 22:23:29 +0900

    [#39/Pratice]레인의 3번째 연습

---
+++ SumSumZip/MainComponents/Views/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  SumSumZip
+//
+//  Created by heesohee on 5/16/24.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text("hello rain!3")
+    }
+}
+
+#Preview {
+    MessageView()
+}
