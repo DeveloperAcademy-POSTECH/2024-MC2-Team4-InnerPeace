095c3536c216d900d81127a5c006b3044658e026
heexohee <rsoy2918@naver.com>
2024-05-16 15:46:19 +0900

    [#20/feature]연습

---
+++ SumSumZip/Helpers/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  SumSumZip
+//
+//  Created by heesohee on 5/16/24.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text("하이루 레인이에욤!")
+    }
+}
+
+#Preview {
+    MessageView()
+}
