73988a2542c2097dc415534f00287d3f5367def7
Bright-jeans <Bright.jjeans@gmail.com>
2024-05-17 07:12:03 +0900

    [#32/Leo]커커밋 연습 2번입니다.

---
+++ Leo_Test_2.swift
@@ -0,0 +1,18 @@
+//
+//  Leo_Test_2.swift
+//  SumSumZip
+//
+//  Created by Leo Yoon on 5/17/24.
+//
+
+import SwiftUI
+
+struct Leo_Test_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Leo_Test_2()
+}
