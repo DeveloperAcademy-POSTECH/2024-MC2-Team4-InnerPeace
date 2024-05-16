08e3254af01f326956861d1acb872c67df22b443
jutamin <wonjy0307@gmail.com>
2024-05-16 17:19:13 +0900

    [#27/연습] 데이지 연습

---
+++ SumSumZip/Daisy.swift
@@ -0,0 +1,18 @@
+//
+//  Daisy.swift
+//  SumSumZip
+//
+//  Created by 원주연 on 5/16/24.
+//
+
+import SwiftUI
+
+struct Daisy: View {
+    var body: some View {
+        Text("Hello, Daisy")
+    }
+}
+
+#Preview {
+    Daisy()
+}
