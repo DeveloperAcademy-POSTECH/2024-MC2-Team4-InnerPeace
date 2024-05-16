54c9ca256500e9ee70124691ad2dec8771cb0c4a
Bright-jeans <Bright.jjeans@gmail.com>
2024-05-16 17:09:29 +0900

    [Leo/#23]커밋합니다.

---
+++ LeoView.swift
@@ -0,0 +1,18 @@
+//
+//  LeoView.swift
+//  SumSumZip
+//
+//  Created by Leo Yoon on 5/16/24.
+//
+
+import SwiftUI
+
+struct LeoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LeoView()
+}
