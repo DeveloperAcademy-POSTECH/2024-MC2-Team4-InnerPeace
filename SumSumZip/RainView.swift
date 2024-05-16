00137e764242045b2fef4c26f07ad8ed2d51904c
heexohee <rsoy2918@naver.com>
2024-05-16 16:28:48 +0900

    [#24/RAIN연습]

---
+++ SumSumZip/RainView.swift
@@ -0,0 +1,18 @@
+//
+//  RainView.swift
+//  SumSumZip
+//
+//  Created by heesohee on 5/16/24.
+//
+
+import SwiftUI
+
+struct RainView: View {
+    var body: some View {
+        Text("Hello Rain!")
+    }
+}
+
+#Preview {
+    RainView()
+}
