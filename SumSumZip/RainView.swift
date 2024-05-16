db7ae7e3aa70808522553b076fadc9c24600f5d8
heexohee <rsoy2918@naver.com>
2024-05-16 17:00:25 +0900

    [#29/레인의 2번 째 연습]

---
+++ SumSumZip/RainView.swift
@@ -0,0 +1,18 @@
+//
+//  RainView.swift
+//  SumSumZip
+//
+//  Created by heesohee on 5/16/24.
+//
+
+import SwiftUI
+
+struct RainView: View {
+    var body: some View {
+        Text("ㅇㅅㅇ 소희")
+    }
+}
+
+#Preview {
+    RainView()
+}
