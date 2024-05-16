12ab7c4f2818701d013e8f2f2a0d931a9b13c88b
heexohee <rsoy2918@naver.com>
2024-05-16 22:33:36 +0900

    [#42/Pratice]레인이 뭔가를 수정해봄, 메인 브랜치 밑에 투두를 파서!

---
+++ SumSumZip/MainComponents/Views/SwiftUIView2.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView2.swift
+//  SumSumZip
+//
+//  Created by heesohee on 5/16/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("edlkfanlng")
+    }
+}
+
+#Preview {
+    SwiftUIView2()
+}
