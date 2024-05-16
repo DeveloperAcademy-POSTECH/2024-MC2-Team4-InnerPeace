75e5b373810e4946b38da16fdbda386f406a3815
Bright-jeans <Bright.jjeans@gmail.com>
2024-05-17 08:52:07 +0900

    [#45/Leo]develop으로 브랜치가 잘 딸려가는지 테스트입니다.

---
+++ SumSumZip/MainComponents/Views/LeoView.swift
@@ -0,0 +1,18 @@
+//
+//  LeoView.swift
+//  SumSumZip
+//
+//  Created by Leo Yoon on 5/17/24.
+//
+
+import SwiftUI
+
+struct LeoView: View {
+    var body: some View {
+        Text("여기도 문제없겠져?")
+    }
+}
+
+#Preview {
+    LeoView()
+}
