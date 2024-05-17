2775bcb88e302fd8453381ce76d473d6491dd328
dave.kang <gang601@naver.com>
2024-05-17 10:26:15 +0900

    feat: SOS View UI 구현 #48

---
+++ SumSumZip/MainComponents/Views/SOSView_NoGreed_Test.swift
@@ -0,0 +1,18 @@
+//
+//  SOSView_NoGreed_Test.swift
+//  SumSumZip
+//
+//  Created by Dongin Kang on 5/17/24.
+//
+
+import SwiftUI
+
+struct SOSView_NoGreed_Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SOSView_NoGreed_Test()
+}
