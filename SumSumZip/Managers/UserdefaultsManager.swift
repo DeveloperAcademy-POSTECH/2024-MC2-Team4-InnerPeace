92d70fed2869cbe554b469ae682631a783114a31
seongahshin <cindy903c@naver.com>
2024-05-22 16:26:31 +0900

    feat: PatientInfoView UI 및 데이터 저장 로직 구현 #128

---
+++ SumSumZip/Managers/UserdefaultsManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserdefaultsManager.swift
+//  SumSumZip
+//
+//  Created by 신승아 on 5/22/24.
+//
+
+import Foundation
---
+++ SumSumZip/Resources/Components/CustomXButton.swift
@@ -0,0 +1,8 @@
+//
+//  CustomXButton.swift
+//  SumSumZip
+//
+//  Created by 신승아 on 5/22/24.
+//
+
+import Foundation
