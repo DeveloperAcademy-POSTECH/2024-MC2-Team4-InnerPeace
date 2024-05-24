30417f7e58ee511f6d953a9a3bc03c7ea4e06dfa
seongahshin <cindy903c@naver.com>
2024-05-24 10:35:32 +0900

    bug: 구조타이밍 설정으로 SOS 알림 변경

--- SumSumZip/MainComponents/Views/SummaryView.swift
+++ SumSumZip/MainComponents/Views/SummaryView.swift
@@ -304,7 +304,7 @@ struct SummaryView: View {
                                 EmergencyLiveActivityManager.shared.startActivity(
                                     title: Strings.LiveActivityView.title,
                                     firstSubtitle: Strings.LiveActivityView.firstSubtitle,
-                                    secondSubtitle: Strings.LiveActivityView.secodSubtitle, isPresented: $isPresented, duration: selectedTime)
+                                    secondSubtitle: Strings.LiveActivityView.secodSubtitle, isPresented: $isPresented, duration: waitingTime)
                             } label: {
                                 
                                 VStack{
@@ -326,7 +326,7 @@ struct SummaryView: View {
                                     .clipShape(RoundedRectangle(cornerRadius: 90, style: .circular))
                                 
                             }
-                            .alert("\(selectedTime)초 뒤 시작", isPresented: $alertShowing) {
+                            .alert("\(waitingTime)초 뒤 시작", isPresented: $alertShowing) {
                                 Button("취소", role: .cancel) {
                                     EmergencyLiveActivityManager.shared.endAllActivities()
                                 }
@@ -336,7 +336,7 @@ struct SummaryView: View {
                                     isPresented = true
                                 }
                             } message: {
-                                Text("\(selectedTime)초 뒤 자동으로 SOS 알람이 시작됩니다.")
+                                Text("\(waitingTime)초 뒤 자동으로 SOS 알람이 시작됩니다.")
                             }
                         }
                         .fullScreenCover(isPresented: $isPresented) {
